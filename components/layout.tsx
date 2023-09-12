d67aab5775d366252e286de78f42137daa200f3b
cynthiajweaverdev <132491873+CynthiaWeaver-Jeannine@users.noreply.github.com>
2023-09-12 05:32:15 -0400

    pass pageId to layout and navbar components; add isSelected

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,25 +1,32 @@
 
+import React from 'react'
+
+
 type NavItemProps = {
     title: string
     url: string
-    }
-
-function NavItem({title, url}: NavItemProps) { 
+isSelected: boolean
+}
+function NavItem(props: NavItemProps) { 
+    const { title, url, isSelected } = props;
     return (     
     <li>       
-        <a className="block px-3 py-2 transition hover:text-teal-500" href={url}>{title}</a>     
+        <a className={`block px-3 py-2 transition hover:text-teal-500 ${isSelected?"text-teak-500": ""}`} href={url}>{title}</a>     
         </li>   
         ) 
     } 
 
-function Navbar() { 
+    type NavbarProps = {
+        pageId: string
+    }
+function Navbar(props: NavbarProps ) { 
     return ( 
     <div className="flex justify-center mx-auto max-w-7xl h-16 pt-6"> 
         <nav> 
             <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur">           
-                <NavItem  title={"About Me"} url="/" />           
-                <NavItem title={"Projects"} url={"/projects"} />           
-                <NavItem title={"Blog"} url={"/blog"} />  
+                <NavItem  title={"About Me"} url="/" isSelected={props.pageId == "about_me"} />           
+                <NavItem title={"Projects"} url={"/projects"} isSelected={props.pageId == "projects"} />           
+                <NavItem title={"Blog"} url={"/blog"} isSelected={props.pageId == "blog"} />  
             </ul> 
         </nav> 
     </div> ) }
@@ -27,7 +34,7 @@ function Navbar() {
 export default function Layout({children}: any) {
     return (
         <>
-        <Navbar />
+        <Navbar pageId={children.props.pageId}/>
         <main>{children}</main>
         </>
     )
